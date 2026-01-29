37ded08735b1db9105358f6ce92e195cca427ada
rodrigo <rodrigocassolp@gmail.com>
2026-01-29 15:59:25 -0300

    Atividade 5. Uso do IS que difere do ==

---
+++ curso-python-1-aula_3/praticando for_while/Atividade5.py
@@ -0,0 +1,13 @@
+"""
+Ana está desenvolvendo seu portfólio para exibir os projetos de Python que concluiu. Ela organizou uma lista com o
+nome de cada projeto, mas percebeu que alguns itens podem estar ausentes, aparecendo como None:
+
+"""
+
+projetos = ["website", "jogo", "análise de dados", None, "aplicativo móvel"]
+
+for projeto in projetos:
+    if projeto is None:
+        print('Projeto ausente.')
+    else:
+        print(projeto)
