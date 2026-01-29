1cebf1b05205616d46829a46a965c06292b27c73
rodrigo <rodrigocassolp@gmail.com>
2026-01-29 15:40:19 -0300

    Atividade 4. Calculando soma de números

---
+++ curso-python-1-aula_3/praticando for_while/Atividade4.py
@@ -0,0 +1,14 @@
+"""
+Você está recebendo uma lista de valores representando os produtos de sua loja virtual e gostaria de calcular a
+soma total desses produtos para entender o desempenho financeiro semanal.
+
+"""
+
+numeros = [10, 20, 30, 40, 50]
+
+soma = 0
+
+for numero in numeros:
+    soma += numero
+
+print(f'A soma total das receitas é: {soma}')
